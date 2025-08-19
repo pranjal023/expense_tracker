8a8ec1ec37fc64bcf1fffaedbacec85ccba4b9c6
pranjal023 <123534075+pranjal023@users.noreply.github.com>
2025-08-19 04:05:09 -0400

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,6 +2,6 @@
 const isProd = location.hostname.endsWith('netlify.app') || location.hostname.endsWith('yourdomain.com');
 window.APP_CONFIG = {
   API_BASE_URL: isProd
-    ? "http://expensetracker-production-3265.up.railway.app"
+    ? "https://expensetracker-production-3265.up.railway.app"
     : "http://localhost:3000"
 };
