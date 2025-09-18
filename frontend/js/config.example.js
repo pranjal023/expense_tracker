a74ee7c9ccc36cb4d0e8049bd1b2668ecd820716
pranjal023 <123534075+pranjal023@users.noreply.github.com>
2025-09-18 11:06:33 -0400

    Update config.example.js

--- frontend/js/config.example.js
+++ frontend/js/config.example.js
@@ -1,2 +1,2 @@
 
-window.APP_CONFIG = { API_BASE_URL: 'https://expensetracker-production-3265.up.railway.app' }; // railway 
+window.APP_CONFIG = { API_BASE_URL: 'https://expense-tracker-ijex.onrender.com' }; 
